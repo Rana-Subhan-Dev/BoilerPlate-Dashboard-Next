b5656b14017770711bbf60ac3f095b9ec0c22372
Rana-Subhan-Dev <ranasubhandev@paklogics.com>
2025-03-14 18:53:26 +0500

    chor: replaced meta tag content

--- src/components/Dashboard/E-commerce.tsx
+++ src/components/Dashboard/E-commerce.tsx
@@ -1,10 +1,10 @@
 "use client";
 import React from "react";
-import ChartThree from "../Charts/ChartThree";
+// import ChartThree from "../Charts/ChartThree";
 import ChartTwo from "../Charts/ChartTwo";
-import ChatCard from "../Chat/ChatCard";
-import TableOne from "../Tables/TableOne";
-import MapOne from "../Maps/MapOne";
+// import ChatCard from "../Chat/ChatCard";
+// import TableOne from "../Tables/TableOne";
+// import MapOne from "../Maps/MapOne";
 import DataStatsOne from "@/components/DataStats/DataStatsOne";
 import ChartOne from "@/components/Charts/ChartOne";
 
